cd08709fa9f0ea4214d0595dacbc23fc495c3aed
Allan Roberto Da Silva Gomes <allanrs@ps.grupopanvel.com.br>
2025-10-03 19:44:45 -0300

    feat: thema dark variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,21 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        'bg-primary': 'var(--bg-primary)',
+        'bg-secondary': 'var(--bg-secondary)',
+        'bg-tertiary': 'var(--bg-tertiary)',
+        'text-primary': 'var(--text-primary)',
+        'text-secondary': 'var(--text-secondary)',
+        'text-tertiary': 'var(--text-tertiary)',
+        'border-primary': 'var(--border-primary)',
+        'border-secondary': 'var(--border-secondary)',
         primary: {
-          DEFAULT: '#F24D0D',
-          dark: '#d64309',
+          DEFAULT: 'var(--color-primary)',
+          hover: 'var(--color-primary-hover)',
         }
+      },
+      backgroundColor: {
+        'app': 'var(--bg-app)',
       }
     },
   },
